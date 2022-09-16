218de856dd0ddaed007b19e64d545a922f04f30d
Storm-Julian Tersteeg (student) <s1155443@student.windesheim.nl>
2022-09-16 10:26:10 +0200

    init

---
+++ ExtendedList/EList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendedList
+{
+    internal class EList<T> : List<T>
+    {
+        public override string ToString()
+        {
+            return String.Join(", ", this);
+        }
+        
+        public R Reduce<R>(Func<R, T, R> f, R initial_state)
+        {
+            R result = initial_state;
+            foreach (T item in this) { result = f(result, item); }
+            return result;
+        }
+    }
+}
---
+++ ExtendedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using ExtendedList;
+
+EList<string> letters = new EList<string>(){"a", "b", "c"};
+
+string letters_combined = letters.Reduce(
+    (t, i) => {t = t + i; return t;},
+    ""
+);
+
+EList<string> letters_uppercased = letters.Reduce(
+    (t, i) => { t.Add(i.ToUpper()); return t; },
+    new EList<string>()
+);
+
+int letters_count = letters.Reduce(
+    (t, i) => { t++; return t; },
+    0
+);
+
+Console.WriteLine(letters_combined);
+Console.WriteLine(letters_uppercased);
+Console.WriteLine(letters_count);
